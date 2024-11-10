f3ab6ffb32c2dea19c09e96b01671467eca37aa8
Yasas Aththanayaka <Aththanayakayasas@gmail.com>
2024-11-10 20:00:16 +0530

    A Fizz Buzz game
      here a Boolean Expressions Are Made to reduce the memory usage and improve the usability

---
+++ Fuzz Game C#/Fuzz Game C#/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuzz_Game_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            /*Create a For Loop 1 to  X {15} = (number needed)
+             *  3 and 5 = FizzBuzz
+             *  %3  = Fizz
+             *  %5 = Buzz
+             *  else = number */
+
+
+            bool divBy3 = false;
+            bool divBy5 = false;
+
+            for (int i = 1; i <= 15; i++) {
+
+                divBy3 = i % 3 == 0;
+                divBy5 = i % 5 == 0;
+
+                if (divBy3 && divBy5) {
+                    Console.WriteLine("FizzBuzz !!");
+                }
+                else if (divBy3) {
+                    Console.WriteLine("Fizz");
+                }
+                else if (divBy5) {
+                    Console.WriteLine("Buzz");
+                }
+                else {
+                    Console.WriteLine(i);
+                }
+
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
